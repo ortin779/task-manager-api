31b8aba8e7d82425badc810409df96efeaa25f2c
Naresh C <chakali.naresh@thoughtworks.com>
2022-02-03 23:00:57 +0530

    Naresh | Update Schema using mongoose validations and validator library

--- src/db/mongoose.ts
+++ src/db/mongoose.ts
@@ -1,27 +1,80 @@
 import mongoose, {Schema} from 'mongoose'
-import {ITask} from "../model/models";
+import validator from "validator";
+import {ITask, IUser} from "../model/models";
 
 mongoose.connect('mongodb://localhost:27017/task-manager-api')
 
 
-const taskSchema = new Schema({
-    description:{
-        type:String
+const userSchema = new Schema({
+    name:{
+        type:String,
+        trim:true,
+        required:true
     },
-    isCompleted:{
-        type:Boolean
+    email:{
+        type:String,
+        trim:true,
+        required:true,
+        validate:(email:string)=>{
+            return validator.isEmail(email)
+        }
+    },
+    password:{
+        type:String,
+        trim:true,
+        required:true,
+        minlength:6,
+        validate:(password:string)=>{
+            if(password.toLowerCase().includes("password")){
+                throw new Error("Password Can't be 'password'")
+            }
+        }
+    },
+    age:{
+        type:Number,
+        default:0,
+        validate:(age:number)=>{
+            if(age<0){
+                throw new Error("Invalid Age value")
+            }
+        }
     }
 })
 
-const Task = mongoose.model<ITask>('Task',taskSchema)
+const User = mongoose.model<IUser>('User',userSchema)
 
-const task1 = new Task({
-    description:"Task-1",
-    isCompleted:false
+// const naresh = new User({
+//     name:"Naresh",
+//     email:"naresh@email.com",
+//     password:"hello there"
+// })
+//
+// naresh.save().then((response)=>{
+//     console.log(response)
+// }).catch((error)=>{
+//     console.log("Error occurred while Saving the User : ",error)
+// })
+
+const taskSchema = new Schema<ITask>({
+    description:{
+        type:String,
+        required:true,
+        trim:true
+    },
+    completed:{
+        type:Boolean,
+        default:false
+    }
 })
 
-task1.save().then((response)=>{
+const Task = mongoose.model("Task",taskSchema)
+
+const newTask = new Task({
+    description:"This is my new Task"
+})
+
+newTask.save().then((response)=>{
     console.log(response)
 }).catch((error)=>{
-    console.log("Error occurred while Saving the Task : ",error)
+    console.log("Error occurred : ",error)
 })
--- src/model/models.ts
+++ src/model/models.ts
@@ -1,5 +1,7 @@
 export interface IUser{
     name:string,
+    email:string,
+    password:string,
     age:number
 }
 
