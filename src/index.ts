61ed9a7389c1f887e147bd6bd6f0d5d38bbf9ffb
Naresh C <chakali.naresh@thoughtworks.com>
2022-02-04 21:17:40 +0530

    Naresh | add endpoint for task creation

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import {connectToDatabase} from "./db/mongo";
 import {User} from "./model/user";
+import {Task} from "./model/task";
 
 const app = express()
 const port = process.env.PORT || 3000
@@ -16,10 +17,23 @@ app.post("/users",((req, res) => {
     }).catch((err)=>{
         res.status(400)
         res.send({
-            "error":"something wrong"
+            "error":err
         })
     })
 }))
+
+app.post("/tasks",(req, res) => {
+    const task = new Task(req.body);
+    task.save().then((response)=>{
+        res.status(201);
+        res.send(response);
+    }).catch((error)=>{
+        res.status(400);
+        res.send({
+            error:error
+        })
+    })
+})
 
 app.listen(port,()=>{
     console.log("Successfully Running on PORT : ",port)
